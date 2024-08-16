a9ac5bdfb38f9a345f8b562450a7217dd5e4c9b6
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-08-16 21:55:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#替换luci源
+sed -i 's|^src-git luci https://github.com/coolsnowwolf/luci|#src-git luci https://github.com/coolsnowwolf/luci|' feeds.conf.default
+sed -i 's|^#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05|src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05|' feeds.conf.default
